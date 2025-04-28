f59a7be033831215a13d81c2fa30914eb979ebad
shaharyar522 <shaharyar.khan4511@gmail.com>
2025-04-28 20:03:15 +0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- FontAwesome Link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Dashboard</title>
+
+
+</head>
+
+<body>
+    <header class="header">
+        <h2 class="u-name">SIDE <b>BAR</b></h2>
+        <i class="fa fa-user" aria-hidden="true"></i>
+    </header>
+
+    <div class="body">
+        <nav class="side-bar">
+            <div class="user-p">
+                <img src="images/admin.jpg" alt="">
+                <h4>shari</h4>
+                <ul>
+                    <li>
+                        <a href="#">
+                            <i class="fa-solid fa-desktop"></i>
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <section class="section-1">
+            <h1>Well come </h1>
+            <p>#code with shari</p>
+        </section>
+    </div>
+</body>
+
+</html>
